ce4e5a85a3f15de77797e3024b01e280f398621f
bholmesdev <hey@bholmes.dev>
2023-08-17 11:25:00 -0600

    refactor: new colors and kbs

--- components/complexity-line/KBCounter.vue
+++ components/complexity-line/KBCounter.vue
@@ -1,12 +1,13 @@
 <template>
-  <p :class="color" class="transition-colors"><strong>{{ kbs }}</strong>kb</p>
+  <p class="text-gray-500"><span :class="color" class="transition-colors"><strong>{{ kbs }}</strong>kb</span> client JS
+  </p>
 </template>
 
 <script setup>
 import { computed, ref } from 'vue';
 
-const kbCounts = [0, 56, 66, 70, 70];
-const colors = ["text-green-500", "text-green-500", "text-yellow-500", "text-orange-500", "text-orange-500"];
+const kbCounts = [0, 66, 76, 80, 80];
+const colors = ["text-green-500", "text-yellow-500", "text-orange-500", "text-red-500", "text-red-500"];
 
 const kbs = computed(() => kbCounts[$slidev.nav.clicks]);
 const color = computed(() => colors[$slidev.nav.clicks]);
